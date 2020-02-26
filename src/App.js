24a5a1b66a32986551d0459458a9b1b281d5efde
Geizer6991 <geizer@ro.ru>
2020-02-26 14:46:36 +0400

    'eee'

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React, {useEffect,useState} from 'react';
+import React, { useState} from 'react';
+import FormData from 'form-data'
+
 import './App.css';
 import axios from "axios";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
-  let [name, setName]=useState([]);
+  let [file, setFile]=useState('');
+  let [fileName, setFileName]=useState('Choose File');
+  let [uploader, setUploader]=useState({});
 
-  useEffect(() => {
-    axios
-        .get('http://localhost:5000')
-        .then(response => {
-          setName(response.data)
-        })
-  }, []);
+  const onChange= e =>{
+      setFile(e.target.files[0]);
+      setFileName(e.target.files[0].name);
+  };
 
-  return <BrowserRouter basename={process.env.PUBLIC_URL}>
-                          <div>
-                              {name}
-                          </div>
-      </BrowserRouter>
+const _handleSubmit= async e=> {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append('file', file);
 
+    try {
+        const res = await axios.post('/save', formData, {
+            headers: {'Content-Type': "multipart/form-data"}
+        });
+        const {fileName, filePath} = res.data
+        setUploader({fileName, filePath})
+    } catch (err) {
+        if (err.response.status === 500) {
+            console.log('there')
+        } else {
+            console.log(err.response.data.msg)
+        }
+    }
+};
 
+return (<BrowserRouter basename={process.env.PUBLIC_URL}>
+            <div>
+                <form onSubmit={_handleSubmit}>
 
+                    <input type="file" name="file" onChange={ onChange }/>
+                    <label type={'submit'} onClick={_handleSubmit} >{fileName}</label>
 
+                    <input type={'submit'} value={'UpLoad'}/>
 
-}
-export default App;
+                </form>
+                <div>
+                    <img src={uploader.filePath} alt="Pic"/>
+                </div>
+            </div>
+        </BrowserRouter>
+)
+}export default App;
